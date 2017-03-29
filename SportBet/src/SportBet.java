03bdf17fcc3c949019b0a8275c8f04ef68dbda80
bbsuser <bbsuser@E304-5>
2017-03-29 16:44:01 +0200

    kleine fixes

--- SportBet/src/SportBet.java
+++ SportBet/src/SportBet.java
@@ -8,15 +8,17 @@ public class SportBet {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
 		int arr1[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18 };
-
+		
+		
 		shuffleArray(arr1);
 		for (int i = 0; i < arr1.length; i++) {
 			System.out.print(arr1[i] + " ");
 		}
 		System.out.println();
 	}
+
 
 	static int Spiel1(int arr1[]) {
 		int team1 = arr1[0];
