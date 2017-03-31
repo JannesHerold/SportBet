2edf4e3f082fd964ebef732ceff280505a9491ba
bbsuser <bbsuser@E201-14>
2017-03-31 12:33:28 +0200

    db weitergemacht

--- SportBet/src/SportBet_DB.java
+++ SportBet/src/SportBet_DB.java
@@ -1,73 +1,159 @@
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+// Standardpaketanwendung - default package application 
+//package SportBet;									
 
+// MySQL-Bibliothek für die Verbindung
+//-MySQL Library for connection Statements
+import java.sql.Connection;												
+// MySQL-Bibliothek mit Treibern, um eine Verbindung zu einer MySQL-Datenbank zu erhalten 
+//-MySQL Library with drivers to get a connection to a MySQL Database
+import java.sql.DriverManager;											
+// MySQL-Bibliothek für Strings mit SQL-Befehlen
+//-MySQL Library for Strings with SQL commands
+import java.sql.PreparedStatement;
+// MySql-Bibliothek, um alle Informationen aus einer Tabelle auszugeben
+//-MySql Library to give all information from a table
+import java.sql.ResultSet;
+// MySQL-Bibliothek, um die Tabelle als Array-Liste auszugeben
+//-MySQL Library to give out the Table as Array List
+import java.util.ArrayList;
+
+
+import java.util.*;
+
+// Start mit der Datenbankklasse, 4 Methoden 
+//-start with the Database class, 4 methods
 public class SportBet_DB {
 
-	public static void main(String[] args) throws SQLException {
-		// TODO Auto-generated method stub
-		// TODO Auto-generated method stub
-		
-		
-				// Diese Eintraege werden zum
-				// Verbindungsaufbau benoetigt.
-				final String hostname = "localhost";
-				final String port = "3306";
-				final String dbname = "mysql";
-				final String user = "root";
-				final String password = "";
-				Connection conn = null;
-				Statement query;
-				String sql;
-				// Java-Database-Connectivity-Driver (JDBC-Treiber) laden
+	// Methode um die Verbindung mit der MySQL-Datenbank mit dem Namen "SportBet" herzustellen
+	//-method that connect to the MySQL database called "SportBet"
+				public static Connection getConnection() throws Exception {
+
+					
+					    
+					
+						// Die Folgenden Einträge werden 
+						// zum Verbindungsaufbau benötigt
+					
+					try {
+						// lädt den Java MySQL Connector Treiber
+						String driver = "com.mysql.jdbc.Driver";
+						// Dieser String gibt den Pfad an, wo die MySQL-Datenbank gespeichert ist
+						String url = "jdbc:mysql://localhost/sportbet";
+						// String, der den Benutzernamen an die Datenbank weitergibt, um Zugriff zu erhalten
+						String username = "root";
+						// String, der das Passwort an die Datenbank weitergibt, um Zugriff zu erhalten
+						String password = "";
+						Class.forName(driver);
+						// Hier wird mit den Informationen URL, username und password versucht,
+						// sich mit der Datenbank zu verbinden
+						Connection conn = DriverManager.getConnection(url, username, password);
+						// Es wird ausgegeben, das sich das Programm mit der Datenbank verbunden hat
+						System.out.println("Verbunden");
+						// Gibt die url, username und das password zurück
+						return conn;
+						// bei bestehenden Fehlern werden diese ausgegeben
+					} catch (Exception e) {
+						// Gibt aus welcher MySQL Datenbank fehler vorliegt
+						System.out.println(e);
+					}
+
+						// Gibt keine Werte zurück wenn Fehler auftreten
+					return null;
+				}
+
+				
+					//
+				public static void createTable() throws Exception {
+						
+					
+					
+					
+					
+						// Die Folgenden Einträge überprüfen,
+						// ob bereits eine Tabelle vorhanden ist.
+						// Wenn dies nicht der Fall ist, wird eine neue Tabelle erstellt
+				
+				
 				try {
-				System.out.println("* Treiber laden");
-				 Class.forName("org.gjt.mm.mysql.Driver").newInstance();
-				 System.out.println(Class.forName("org.gjt.mm.mysql.Driver"));
-				 System.out.println(Class.forName("org.gjt.mm.mysql.Driver").newInstance());
+						Connection con = getConnection();
+						PreparedStatement create = con.prepareStatement("CREATE TABLE IF NOT EXISTS logindaten(id int NOT NULL AUTO_INCREMENT, username varchar(255), password varchar(255), PRIMARY KEY (id))");
+						create.executeUpdate();
+
+
+					} catch (Exception e) {
+						System.out.println(e);
+					} finally {
+						System.out.println("Tabelle Erstellt oder Vorhanden!");
+					}
 				}
-				catch (Exception e) {
-				System.err.println("Unable to load driver.");
-				e.printStackTrace();
-				}
-				//DB-Verbindung aufbauen
+	
+				
+				
+				
+				
+				
+	public static Scanner eingabe;
+
+	public static void main(String[] args) throws Exception {
+
+
+
+
+			 createTable();
+			 post();
+			 get();
+	}
+
+
+			public static ArrayList<String> get() throws Exception {
 				try {
-				 System.out.println("* Verbindung aufbauen");
-				 String url = "jdbc:mysql://"+hostname+":"+port+"/"+dbname;
-				 conn = DriverManager.getConnection(url, user, password);
+					Connection con = getConnection();
+					PreparedStatement statement = con.prepareStatement("SELECT username, password FROM logindaten");
+
+					ResultSet result = statement.executeQuery();
+
+					ArrayList<String> array = new ArrayList<String>();
+					while (result.next()) {
+						System.out.print(result.getString("username"));
+						System.out.print(" ");
+						System.out.println(result.getString("password"));
+
+						array.add(result.getString("password"));
+					}
+					System.out.println("Alle Inhalte wurden wiedergegeben");
+					return array;
+
+				} catch (Exception e) {
+					System.out.println(e);
 				}
-				catch (SQLException sqle) {
-				System.out.println("SQLException: " + sqle.getMessage());
-				System.out.println("SQLState: " + sqle.getSQLState());
-				System.out.println("VendorError: " + sqle.getErrorCode());
-				sqle.printStackTrace();
+				return null;
+
+			}
+
+			public static void post() throws Exception{
+				eingabe = new Scanner (System.in);
+				System.out.print("Benutzername: ");
+				String var1 = eingabe.nextLine();
+				System.out.print("\nPasswort: ");
+				String var2 = eingabe.nextLine();
+
+				try {
+					Connection con = getConnection();
+					PreparedStatement posted = con.prepareStatement("INSERT INTO logindaten (username, password) VALUES ('" + var1 + "', '" + var2 + "')");
+					posted.executeUpdate();
+				} catch (Exception e) {
+					System.out.println(e);
+				} finally {
+					System.out.println("Eingabe erfolgreich");
 				}
-				// Tabelle anzeigen
-//				try {
-//				// SQL-Abfrage generieren
-//				query = conn.createStatement();
-//				 sql = "SELECT * FROM Fahrzeuge WHERE Abgasnorm='EU5'";
-//				 ResultSet result = query.executeQuery(sql);
-//				 // Ergebnisstabelle durchsuchen
-//				 while (result.next()) {
-//				String modell = result.getString("Modell");
-//				String hersteller = result.getString("Hersteller");
-//				String stammsitz = result.getString("Stammsitz");
-//				String abgasnorm = result.getString("Abgasnorm");
-//				String info = modell + ", " + hersteller + ", " + stammsitz + ", " + abgasnorm;
-//				System.out.println(info);
-//				 }
-//				}
-//				catch (SQLException e) {
-//				e.printStackTrace();
-//				}
-				 // Datenbank-Verbindung beenden
-				 System.out.println();
-				 System.out.println("* Datenbank-Verbindung beendet");
-				 conn.close();
+			}
+
+			
+
+			
 
 	}
 
-}
+
+
+
